505d09f3880f0c8652af98a2a08dbac1b7e0c4c1
atans2468 <austintans7@gmail.com>
2018-04-10 09:59:12 -0700

    updating live, work, play again

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -61,6 +61,41 @@ unionVillage.controller("pricingCtrl", function($scope, $state){
 	
 });
 
+unionVillage.controller("successCtrl", function($scope, $state){
+	var $cell = $('.card');
+	
+	//open and close card when clicked on card
+	$cell.find('.js-expander').click(function() {
+	
+	  var $thisCell = $(this).closest('.card');
+	
+	  if ($thisCell.hasClass('is-collapsed')) {
+		$cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+		$thisCell.removeClass('is-collapsed').addClass('is-expanded');
+		
+		if ($cell.not($thisCell).hasClass('is-inactive')) {
+		  //do nothing
+		} else {
+		  $cell.not($thisCell).addClass('is-inactive');
+		}
+	
+	  } else {
+		$thisCell.removeClass('is-expanded').addClass('is-collapsed');
+		$cell.not($thisCell).removeClass('is-inactive');
+	  }
+	});
+	
+	//close card when click on cross
+	$cell.find('.js-collapser').click(function() {
+	
+	  var $thisCell = $(this).closest('.card');
+	
+	  $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+	  $cell.not($thisCell).removeClass('is-inactive');
+	
+	});
+});
+
 unionVillage.controller("contactCtrl", function($scope, $state){
 	$('.maps').click(function () {
 		$('.maps iframe').css("pointer-events", "auto");
